1c6212fa3ccb0497e1db95e5c79795c6cb12a92a
Andru Stefanescu <als209@cam.ac.uk>
2022-01-21 23:22:28 +0000

    Simple structure
    Signed-off-by: Andru Stefanescu <als209@cam.ac.uk>

---
+++ include/smkhe/initial.h
@@ -0,0 +1,8 @@
+//
+// Created by Andru Stefanescu on 21.01.2022.
+//
+
+#ifndef SMKHE_INITIAL_H
+#define SMKHE_INITIAL_H
+
+#endif //SMKHE_INITIAL_H
---
+++ src/initial.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Andru Stefanescu on 21.01.2022.
+//
+
+#include "smkhe/initial.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello";
+    return 0;
+}
